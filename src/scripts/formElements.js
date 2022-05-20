efc4216c643465dc8cf1dbdd36a72242b3e015ae
Shalane Proctor <shalane088@gmail.com>
2022-05-19 20:10:54 -0500

    Modularized code

---
+++ src/scripts/formElements.js
@@ -0,0 +1,47 @@
+import renderToDOM from './renderToDom';
+import { students, houses } from './data';
+import studentsOnDom from './studentOnDom';
+
+const createId = (array) => {
+  if (array.length) {
+    const idArray = array.map((el) => el.id);
+    return Math.max(...idArray) + 1;
+  } return false;
+};
+const sortStudent = (e) => {
+  e.preventDefault();
+  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+  if (e.target.id === 'sorting') {
+    const student = document.querySelector('#student-name');
+
+    // create the new student object
+    students.push({
+      id: createId(students),
+      name: student.value,
+      house: sortingHat.house,
+      crest: sortingHat.crest,
+    });
+
+    student.value = ''; // reset value of input
+    studentsOnDom('#students', students);
+  }
+};
+const form = () => {
+  const domString = `<form id="sorting" class="d-flex flex-column form-floating ">
+    <input
+    type="text"
+    class="form-control mb-1"
+    id="student-name"
+    placeholder="Enter a name"
+    required
+  />
+  <label for="floatingInputValue">Name to be sorted</label>
+<button type="submit" class="btn btn-success">Get Sorted!</button>
+</form>`;
+
+  renderToDOM('#form-container', domString);
+  document.querySelector('#sorting').addEventListener('submit', sortStudent);
+};
+
+export { createId, sortStudent, form };
